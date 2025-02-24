1960f6afd3908292fdf5c89c7c4aab0385fc5544
Evgenii Chernyshov <zidane98@mail.ru>
2025-02-24 08:45:20 +0300

    update cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,8 +5,8 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 @Command(
-name = "gendiff",
-description =  "testing picocli",
+name = "gendiFF",
+description = "Compares two configuration files and shows a difference.",
 mixinStandardHelpOptions = true,
 version = "gendiff 1.0"
 )
@@ -16,8 +16,22 @@ public class App implements Runnable  {
     public void run() {
     }
 
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            defaultValue = "stylish",
+            paramLabel = "format"
+    )
+    private String format;
+
+    @Parameters(index = "0", description ="path to first file", paramLabel = "filepath1")
+    private String filepath1;
+
+    @Parameters(index = "0", description ="path to second file", paramLabel = "filepath2" )
+    private String filepath2;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       // System.out.println("Hello World!");
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
